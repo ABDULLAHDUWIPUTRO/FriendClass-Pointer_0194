afb84e50346f4790e48fb6adde2d54d4f678cb23
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-05-05 10:55:15 +0700

    // Commit 4: tambah method setNama dan friend class siswa pada class orang

--- TEORI/FriendClassSatu.cpp
+++ TEORI/FriendClassSatu.cpp
@@ -1,6 +1,7 @@
 // Commit 1: inisialisasi project
 // Commit 2: buat forward declaration class siswa dan class orang
 // Commit 3: tambah variabel private nama pada class orang
+// Commit 4: tambah method setNama dan friend class siswa pada class orang
 
 #include <iostream>
 #include <string>
@@ -11,4 +12,8 @@ class orang
 {
 private:
     string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
 };
