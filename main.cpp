65b907775fa782fd42a1dded225d2d6e42c6e885
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-05-05 10:33:52 +0700

    // Commit 3:  tambah method input pada LayangLayang

--- main.cpp
+++ main.cpp
@@ -9,3 +9,12 @@ class LayangLayang;
 class LayangLayang {
     private:
         float d1, d2, sisi1, sisi2;
+
+    public:
+        void input() { // Commit 3:  tambah method input pada LayangLayang
+            cout << "-- Input Layang-Layang --" << endl;
+            cout << "Diagonal 1   : "; cin >> d1;
+            cout << "Diagonal 2   : "; cin >> d2;
+            cout << "Sisi pendek  : "; cin >> sisi1;
+            cout << "Sisi panjang : "; cin >> sisi2;
+        }
