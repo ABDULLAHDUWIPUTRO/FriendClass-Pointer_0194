49ec7fb89e3f5312b4951a0fbe2bbdf912c55cb5
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-05-05 10:54:38 +0700

    // Commit 2: buat forward declaration class siswa dan class orang

--- TEORI/FriendClassSatu.cpp
+++ TEORI/FriendClassSatu.cpp
@@ -1,5 +1,11 @@
 // Commit 1: inisialisasi project
+// Commit 2: buat forward declaration class siswa dan class orang
 
 #include <iostream>
 #include <string>
 using namespace std;
+
+class siswa;
+class orang
+{
+};
