24d0458f8aa8d782d0409e0e284262db518b2489
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-05-05 10:33:29 +0700

    // Commit 2:  buat class LayangLayang dengan variabel private

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
 // Commit 1:  inisialisasi project
 #include <iostream>
 using namespace std;
+
+class BelahKetupat;
+class LayangLayang;
+
+// Commit 2:  buat class LayangLayang dengan variabel private
+class LayangLayang {
+    private:
+        float d1, d2, sisi1, sisi2;
